526fd9c3e7012fd9445d21ad007d47e24e6dc540
AmirDiaz <amirsaoudi620@gmail.com>
2025-10-26 11:16:35 +0100

    Update get-orders.js

--- netlify_site/functions/get-orders.js
+++ netlify_site/functions/get-orders.js
@@ -1,1 +1,10 @@
+import { neon } from '@netlify/neon';
 
+export default async () => {
+  const sql = neon(); // الاتصال بقاعدة بيانات Neon المرتبطة بـ Netlify
+  const orders = await sql`SELECT * FROM orders`; // جلب كل الطلبات
+
+  return new Response(JSON.stringify(orders), {
+    headers: { 'Content-Type': 'application/json' },
+  });
+};
